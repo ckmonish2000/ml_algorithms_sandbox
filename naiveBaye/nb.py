ccace471ad4879e27955c99c024e6df3c92591a5
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-24 14:29:04 +0530

    init navie bayes class

---
+++ naiveBaye/nb.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class NaiveBayes:
+    def fit(self,X,y):
+        pass
+
+    def predict(self,X):
+        pass
+
+    def _predict():
+        pass
+
+    def _pdf():
+        pass
