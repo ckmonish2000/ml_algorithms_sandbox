0b0bf3a53968f392877a6cb11398a9c2050a80f0
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-24 15:19:28 +0530

    working test

--- naiveBaye/nb.py
+++ naiveBaye/nb.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class NaiveBayes:
     def fit(self,X,y):
-        n_samples,n_features=x.shape
+        n_samples,n_features=X.shape
         self._classes=np.unique(y)
         n_classes=len(self._classes)
 
