4194023b4657773ad02e652d1d303921a7d4396d
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-24 14:42:13 +0530

    defined fit

--- naiveBaye/nb.py
+++ naiveBaye/nb.py
@@ -3,12 +3,29 @@ import numpy as np
 
 class NaiveBayes:
     def fit(self,X,y):
-        pass
+        n_samples,n_features=x.shape
+        self._classes=np.unique(y)
+        n_classes=len(self._classes)
+
+        #init mean variance and priors
+        self._mean=np.zeros((n_classes,n_features),dtype=np.float64)
+        self._var=np.zeros((n_classes,n_features),dtype=np.float64)
+        self.prior=np.zeros(n_classes,dtype=np.float64)
+
+
+        for c in self._classes:
+            X_c=X[c==y]
+            self._mean[c,:]= X.mean(axis=0)
+            self._var[c,:]= X.var(axis=0)
+            self.prior[c]= X_c.shape[0]/ float(n_samples)
+
+
+
 
     def predict(self,X):
         pass
 
-    def _predict():
+    def _predict(self,x):
         pass
 
     def _pdf():
