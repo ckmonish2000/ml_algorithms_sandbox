333918395d0515b23cd6c728964871a2383cb9e0
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-24 15:18:33 +0530

    init testnb

---
+++ naiveBaye/testnb.py
@@ -0,0 +1,19 @@
+from nb import NaiveBayes
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+def accuracy(pred,label):
+    acc=np.sum(pred==label)/len(label)
+    return acc
+
+X,y=datasets.make_classification(n_samples=1000,n_features=10,n_classes=2,random_state=123)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=123)
+
+
+model=NaiveBayes()
+model.fit(X_train,y_train)
+predict=model.predict(X_test)
+
+acc=accuracy(predict,y_test)
+print(acc)
