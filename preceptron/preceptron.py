56d3881b43d582866f3955e49a5f660f11bb24af
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-27 15:25:22 +0530

    preceptron structure init

--- preceptron/preceptron.py
+++ preceptron/preceptron.py
@@ -1,1 +1,18 @@
 import numpy as np
+
+class Preceptron:
+    def __init__(self,lr=0.001,epochs=10000):
+        self.lr=lr
+        self.epochs=epochs
+        self.activation=self.unit_step_func
+        self.weight=None
+        self.bias=None
+
+    def fit(self,X,y):
+        pass
+
+    def predict(self,X):
+        pass
+
+    def unit_step_func(self,x):
+        return np.where(x>=0,1,0)
